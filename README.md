8d29459b4f69dfd130c55c03b7f7fbb9b1c8ed19
Taras2907 <katrychtaras@gmail.com>
2020-03-02 10:36:12 +0100

    add backend

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # QuestionTime
+Regular questions and answers forum to learn basics of Django Rest-Framework and Vue.js
---
+++ users/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model= CustomUser
+    list_display = ['username', 'email', 'is_staff']
+
+admin.register(CustomUserAdmin)
+
+
